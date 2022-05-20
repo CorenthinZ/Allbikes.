f515c32ba29d72491930e241d98225d071bf09f0
ZPyrolink <73246085+ZPyrolink@users.noreply.github.com>
2022-05-20 15:45:05 +0200

    Create MapUtils.getInverse

--- app/src/main/java/com/pyrolink/allbikes/utils/MapUtils.java
+++ app/src/main/java/com/pyrolink/allbikes/utils/MapUtils.java
@@ -1,5 +1,6 @@
 package com.pyrolink.allbikes.utils;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public final class MapUtils
@@ -13,5 +14,15 @@ public final class MapUtils
                 return entry.getKey();
 
         return null;
+    }
+
+    public static <K, V> Map<V, K> getInverse(Map<K, V> map)
+    {
+        Map<V, K> inverse = new HashMap<>(map.size());
+
+        for (Map.Entry<K, V> entry : map.entrySet())
+            inverse.put(entry.getValue(), entry.getKey());
+
+        return inverse;
     }
 }
